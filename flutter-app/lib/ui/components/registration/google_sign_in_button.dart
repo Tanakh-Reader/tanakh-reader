fcd140e0f69e1f2b170fa07e3769f80b7aca04fb
sethbam9 <54952081+sethbam9@users.noreply.github.com>
2022-12-09 15:21:01 -0500

    Adding registration and authentication features/UI

---
+++ flutter-app/lib/ui/components/registration/google_sign_in_button.dart
@@ -0,0 +1,58 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:tanakhreader/constants.dart';
+import 'package:tanakhreader/utils/authentication.dart';
+
+import '../../screens/profile_screen.dart';
+
+class GoogleSignInButton extends StatefulWidget {
+  @override
+  _GoogleSignInButtonState createState() => _GoogleSignInButtonState();
+}
+
+class _GoogleSignInButtonState extends State<GoogleSignInButton> {
+  bool _isSigningIn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: _isSigningIn
+          ? CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            )
+          : GestureDetector(
+              onTap: () async {
+                setState(() {
+                  _isSigningIn = true;
+                });
+
+                User? user =
+                    await AuthService.signInWithGoogle(context: context);
+
+                setState(() {
+                  _isSigningIn = false;
+                });
+
+                if (user != null) {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => UserInfoScreen(
+                        user: user,
+                      ),
+                    ),
+                  );
+                }
+            
+              },
+              // TODO: add theme conditional 
+              child: Image(
+                      image: lightGoogleSignInButton,
+                      height: 50,
+                    
+                ),
+              ),
+            );
+  }
+}
