c326bfd0031c3756aed45e3cb050ece388da79e2
sethbam9 <54952081+sethbam9@users.noreply.github.com>
2025-04-23 21:15:32 -0400

    cleaning up

--- GPT-4-advice/lib/providers/reading_viewmodel_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:your_app/viewmodels/reading_viewmodel.dart';
-
-final readingViewModelProvider = StateNotifierProvider.autoDispose<ReadingViewModel>((ref) {
-  final initialChapter = ref.watch(initialChapterProvider).state;
-  return ReadingViewModel(ref, currentChapter: initialChapter);
-});
-
-final initialChapterProvider = StateProvider<int>((ref) => 1);
